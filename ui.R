a7690597a00a6a0888f93d235d9b1844bfed8266
LacroixLaurent <laurent.lacroix@inserm.fr>
2017-03-30 15:22:12 +0100

    test

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ ui =fluidPage(
   p('by L. Lacroix, laurent.lacroix@inserm.fr'),
   #hr(),
 
-		wellPanel(textInput("seq",label= h3("Sequence"),value="GGGTTAGGGTTAGGGTTAGGG"),style="background-color:lightgreen;"),
+		wellPanel(textInput("seq",label= h3("Sequence"),value="GGGTTAGGGTTAGGGTTAGGG"),style="background-color:pink;"),
 		wellPanel(h3("Score (length)"),h1(textOutput("text1"),style="color:red;"),style="background-color:lightblue;")
 		)
 
