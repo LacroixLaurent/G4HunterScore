fd76696a98e6a30777e5df7218e83a166b6ed15a
LacroixLaurent <laurent.lacroix@inserm.fr>
2017-03-31 11:49:01 +0100

    add link to readme

--- ui.R
+++ ui.R
@@ -3,7 +3,7 @@ library(shiny)
 ui =fluidPage(
   titlePanel("G4Hunter Score"),
   p('by L. Lacroix, laurent.lacroix@inserm.fr'),
-  #hr(),
+  helpText(a("Click Here to open the README",href="README.html",target="_blank")),
 
 		wellPanel(textInput("seq",label= h3("Sequence"),value="GGGTTAGGGTTAGGGTTAGGG"),style="background-color:pink;"),
 		wellPanel(h3("Score (length)"),h1(textOutput("text1"),style="color:red;"),style="background-color:lightblue;")
